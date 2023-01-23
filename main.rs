45aad9811b980b7a07fe2db69f2dddec743c9b8a
Mikkel <mtkongsted@gmail.com>
2023-01-23 13:35:51 +0100

    refactoring

--- main.rs
+++ main.rs
@@ -1,24 +1,27 @@
-use rand::prelude::*;
+use rand::Rng;
 
+fn random_number_1_to_100() -> u8 {
+    rand::thread_rng().gen_range(0..100)
+}
+
+fn get_input() -> u8{
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    let input = line.trim().parse().unwrap();
+    input
+}
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let random_float_0_to_1: f64 = rng.gen();
-    let random_float_0_to_100: f64 = random_float_0_to_1 * 100.0;
-    let number_to_guess: f64 = random_float_0_to_100.floor();
+    let number_to_guess = random_number_1_to_100();
     let mut number_guessed = false;
     while !number_guessed {
-        let mut line = String::new();
         println!("Guess the number from 0-100");
-        std::io::stdin().read_line(&mut line).unwrap();
-        let number: f64 = line.trim().parse().unwrap();
-        if number > number_to_guess {
+        let guessed_number = get_input();
+        if guessed_number > number_to_guess {
             println!("The number is lower");
-        }
-        else if number < number_to_guess {
+        } else if guessed_number < number_to_guess {
             println!("The number is higher");
-        }
-        else {
+        } else {
             number_guessed = true;
             println!("You guessed the number!");
         }
